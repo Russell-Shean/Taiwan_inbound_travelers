aa3e88381b380821d08c377a17bc3e76a0b1acfd
Russell-Shean <russshean@gmail.com>
2022-01-06 16:36:24 +0800

    selenium start

---
+++ unfinished/rselenium_start.R
@@ -0,0 +1,32 @@
+
+# Selenium start
+
+
+
+library(rvest)
+library(dplyr)
+
+page_r <- read_html("https://www.sportsnet.org.tw/score_detail_utf8.php?Id=250") %>% 
+  html_nodes("a") %>%
+  html_attr("href")
+  html_text()
+
+#intro to R selenium tutorial: https://joshuamccrain.com/tutorials/web_scraping_R_selenium.html
+library("RSelenium")
+
+  #this opens a light version of firefox 
+rD <- rsDriver(browser="firefox", port=4546L, verbose=F)
+
+#this saves the version of firefox that we just opened
+remDr <- rD[["client"]]
+
+
+#this opens the tourism bureau website that we want
+
+remDr$navigate("https://stat.taiwan.net.tw/inboundSearch")
+
+
+zip <- "11010"
+remDr$findElement(using = "id", value = "checkboxform0")$sendKeysToElement(list(zip))
+
+?RSelenium
