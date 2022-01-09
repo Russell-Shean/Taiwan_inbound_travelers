2729090b0e17e5ee2c16e0cb9e7d2d9d62e59f98
Russell-Shean <russshean@gmail.com>
2022-01-09 20:18:48 +0800

    slight graph change

--- visa_category.R
+++ visa_category.R
@@ -87,9 +87,9 @@ labels(visa_categories_melt) <- c("Year","Entries","Visa Category")
 
 # we only really care about post 2015?
 visa_categories_melt %>%
-  filter(year > 2002)%>%     # this filters out anything before or at 2015
+  filter(year > 1980)%>%     # this filters out anything before or at 2015
   ggplot()+                 # this calls the plot using the dataframe you just piped in
-  geom_line(                #geom_line makes a line graph, other geoms make other types of graphs
+  geom_point(                #geom_line makes a line graph, other geoms make other types of graphs
     aes(x=year,             # this makes year the x axis
                 y=value,    # this the number of visitors the y axis
                 color= variable)) + #this stratifies and colors by visa category
